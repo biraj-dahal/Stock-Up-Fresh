a470d2e3dca4c3ded1ebdbcbb436ab0fe33a2bf2
biraj-dahal <dahalbiraj10@gmail.com>
2025-04-05 19:33:14 -0400

    Entry

---
+++ Stock Up Fresh/Stock Up Fresh/Stock_Up_FreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_Up_FreshApp.swift
+//  Stock Up Fresh
+//
+//  Created by Biraj Dahal on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Stock_Up_FreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginScreen()
+        }
+    }
+}
