b655d12f44d8dbe0ec21c596ed5521e6e77623d8
biraj-dahal <dahalbiraj10@gmail.com>
2025-04-05 19:32:21 -0400

    Color Code extension for the app. Beige/Olive/Grey/Black/White

---
+++ Stock Up Fresh/Stock Up Fresh/Extensions/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  Stock Up Fresh
+//
+//  Created by Biraj Dahal on 4/5/25.
+//
+
+import SwiftUI
+
+extension Color {
+    static let appBeige = Color(red: 244 / 255, green: 239 / 255, blue: 231 / 255)
+    static let appOlive = Color(red: 105 / 255, green: 127 / 255, blue: 82 / 255)
+}
