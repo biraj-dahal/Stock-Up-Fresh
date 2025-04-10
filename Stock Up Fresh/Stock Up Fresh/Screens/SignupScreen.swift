dc5a688f8411c3645248fff1da6fc40a03b7de3a
biraj-dahal <dahalbiraj10@gmail.com>
2025-04-09 23:37:11 -0400

    For now public

---
+++ Stock Up Fresh/Stock Up Fresh/Screens/SignupScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SignupScreen.swift
+//  Stock Up Fresh
+//
+//  Created by Biraj Dahal on 4/9/25.
+//
+
+import SwiftUI
+
+struct SignupScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupScreen()
+}
